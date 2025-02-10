e217122837e258487377083591009a37fb28a948
kayailkerugur <kayailkerugur@gmail.com>
2025-02-10 12:13:49 +0300

    Endpoint model update. Adding a request body model has been made easier.

--- Sources/NetSecureKit/Network/Endpoint.swift
+++ Sources/NetSecureKit/Network/Endpoint.swift
@@ -30,7 +30,37 @@ public protocol Endpoint {
     var headers: [String: String]? { get }
     var queryParameters: [String: String]? { get }
     var body: Data? { get }
-} // SPM
+    mutating func makeDataToModel<T: Codable>(model: T)
+}
+
+public struct EndpointModel: Endpoint {
+    public var baseURL: String
+    public var path: String
+    public var method: HTTPMethod
+    public var headers: [String: String]?
+    public var queryParameters: [String: String]?
+    public var body: Data?
+    
+    public init(baseURL: String, path: String, method: HTTPMethod, headers: [String : String]? = nil, queryParameters: [String : String]? = nil, model: Codable? = nil) {
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+        self.headers = headers
+        self.queryParameters = queryParameters
+        if let model = model {
+            self.makeDataToModel(model: model)
+        }
+    }
+    
+    mutating public func makeDataToModel<T: Codable>(model: T) {
+        do {
+            let modelData = try JSONEncoder().encode(model)
+            self.body = modelData
+        } catch {
+            CapsulateLogger.addLog(functionName: #function, message: "Model dataya dönüşümü sırasında hata oluştu, hata: \(error.localizedDescription)")
+        }
+    }
+}
 
 extension Endpoint {
     func urlRequest() throws -> URLRequest {
